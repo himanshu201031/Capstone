ab01fe5e5b492f2d22be9793086b74f9d255e2ba
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-03-20 11:12:14 +0530

    feat: scaffold frontend with Vite, React, Tailwind, Redux placeholder

---
+++ frontend/src/app/components/Placeholder.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Placeholder: React.FC = () => {
+  return <div className="p-4 border border-gray-300 rounded">Placeholder component</div>;
+};
+
+export default Placeholder;
---
+++ frontend/src/app/store/index.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+// TODO: add slice reducers
+const store = configureStore({
+  reducer: {}
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
